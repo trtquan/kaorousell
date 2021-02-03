cc3ba34fa9f0a3a57b078d08598aaccbdb6ce44d
quantran <trtquan.com>
2021-02-03 08:52:36 +0700

    add config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { config } from '../config/config';
 
 function App() {
   return (
---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+import deepFreeze from 'deep-freeze';
+
+
+const conf = {
+    slide_duration: 3000,
+  }
+
+export const config = deepFreeze(conf);
